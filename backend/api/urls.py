e31a721ca961e632693b14f25f82860c018ffeee
duplxey <nick.tomazic@gmail.com>
2020-09-17 18:41:26 +0200

    Added Lecture

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,4 +5,7 @@ from .views import *
 urlpatterns = [
     path('users/', UserList.as_view()),
     path('user/<int:user_id>/', UserInfo.as_view()),
+
+    path('lectures/', LectureList.as_view()),
+    path('lecture/<int:lecture_id>/', LectureInfo.as_view()),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,9 +1,8 @@
-from django.contrib.auth.models import User
 from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from api.models import UserModel
+from api.models import UserModel, Lecture
 
 
 class UserList(APIView):
@@ -30,7 +29,7 @@ class UserInfo(APIView):
     def get(request, user_id):
 
         if not UserModel.objects.filter(id=user_id).exists():
-            raise NotFound(detail="Requested User could not be found.", code=404)
+            raise NotFound(detail="Requested UserModel could not be found.", code=404)
 
         user_model = UserModel.objects.get(id=user_id)
 
@@ -38,5 +37,48 @@ class UserInfo(APIView):
             'id': user_model.id,
             'name': user_model.name,
             'last_name': user_model.last_name,
+        })
+
+
+class LectureList(APIView):
+
+    @staticmethod
+    def get(request):
+        lectures = Lecture.objects.all()
+
+        return Response({
+            'lecture_count': lectures.count(),
+            'lectures': [
+                {
+                    'id': lecture.id,
+                    'title': lecture.title,
+                    'views': lecture.views,
+                    'author': lecture.author.id,
+                    'published': lecture.published,
+                    'video': lecture.video,
+                    'audio': lecture.audio,
+                } for lecture in lectures
+            ]
+        })
+
+
+class LectureInfo(APIView):
+
+    @staticmethod
+    def get(request, lecture_id):
+
+        if not Lecture.objects.filter(id=lecture_id).exists():
+            raise NotFound(detail="Requested Lecture could not be found.", code=404)
+
+        lecture = Lecture.objects.get(id=lecture_id)
+
+        return Response({
+            'id': lecture.id,
+            'title': lecture.title,
+            'views': lecture.views,
+            'author': lecture.author.id,
+            'published': lecture.published,
+            'video': lecture.video,
+            'audio': lecture.audio,
         })
 
