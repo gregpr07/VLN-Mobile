638ae51ef08902797ef4998c99a72d2f5d32f083
gregpr07 <gregor.ninjaboy@gmail.com>
2020-09-22 17:56:41 +0200

    added redux

---
+++ frontend/pages/login.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Button, StyleSheet, View } from "react-native";
+import { connect } from "react-redux";
+import { saveUserToken } from "../services/actions";
+
+class SignInScreen extends React.Component {
+  static navigationOptions = {
+    title: "Please sign in",
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button title="Sign in!" onPress={this._signInAsync} />
+      </View>
+    );
+  }
+
+  _signInAsync = () => {
+    this.props
+      .saveUserToken()
+      .then(() => {
+        console.log("logged in");
+      })
+      .catch((error) => {
+        this.setState({ error });
+      });
+  };
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+const mapStateToProps = (state) => ({
+  token: state.token,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  saveUserToken: () => dispatch(saveUserToken()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignInScreen);
