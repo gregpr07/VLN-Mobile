2c7f93abd8b0fb0fd0c7c14f3604bab3f9184c44
duplxey <nick.tomazic@gmail.com>
2020-10-10 23:08:22 +0200

    Removed unused imports, updated requirements.txt.

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,9 +1,6 @@
-from pprint import pprint
-
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from rest_framework.fields import empty
 
 from api.models import UserModel, Lecture, Slide, Note, Author, Event, Playlist
 
