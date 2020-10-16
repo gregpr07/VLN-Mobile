489f1800ed5da403d310d88a08157ea1bfbb31e9
gregpr07 <gregor.ninjaboy@gmail.com>
2020-10-16 15:36:56 +0200

    fixed getting videos

--- backend/api/management/commands/database_sync.py
+++ backend/api/management/commands/database_sync.py
@@ -42,16 +42,32 @@ conn = psycopg2.connect(conn_string)
 
 cursor = conn.cursor()
 
+cursor.execute("Select * FROM storage_volume LIMIT 0")
+storage_volume_cols = [desc[0] for desc in cursor.description]
+
+
 cursor.execute(
     "SELECT * FROM storage_volume WHERE \"server_id\" = '8'")
 
+
 storage_volumes = {}
 for vol in cursor.fetchall():
-    storage_volumes[vol[0]] = vol[2]
+    storage_volumes[vol[storage_volume_cols.index(
+        'id')]] = vol[storage_volume_cols.index('path')]
 
+# GET COLUMNS OF STORAGE SERVER -> AVOID STATICALLY TYPED INDEXES (IF THEY CHANGE)
+cursor.execute("Select * FROM storage_server LIMIT 0")
+STORAGE_SERVER_COLS = [desc[0] for desc in cursor.description]
+
+# get server host link (have to manually add http://)
 cursor.execute("SELECT * FROM storage_server WHERE \"id\" = '8'")
+SERVER_HOST = 'http://' + cursor.fetchone()[STORAGE_SERVER_COLS.index('host')]
 
-server_host = cursor.fetchone()[2]
+cursor.execute("Select * FROM storage_file LIMIT 0")
+STORAGE_COLS = [desc[0] for desc in cursor.description]
+
+cursor.execute("Select * FROM vl_contribution LIMIT 0")
+AUTHOR_COLS = [desc[0] for desc in cursor.description]
 
 
 class Command(BaseCommand):
@@ -71,40 +87,50 @@ class Command(BaseCommand):
 
         cursor.execute(query)
 
-        author_id = cursor.fetchone()[1]
+        author_id = cursor.fetchone()[AUTHOR_COLS.index('contributor_id')]
 
         return Author.objects.get(id=author_id)
 
     def makeurl(self, video):
-        url = 'http://' + server_host + '/' + \
-            storage_volumes[video[-2]] + '/' + \
-            str(video[-4]) + '/' + video[2] + '.' + video[-3]
+
+        url = SERVER_HOST + '/' + \
+            storage_volumes[video[STORAGE_COLS.index('volume_id')]] + '/' + \
+            str(video[STORAGE_COLS.index('dir')]) + \
+            '/' + video[STORAGE_COLS.index('hash')] + \
+            '.' + video[STORAGE_COLS.index('ext')]
 
         return url
 
     def get_lecture_video(self, slug):
 
         cursor.execute(
-            f"SELECT * FROM storage_file WHERE \"path\"::TEXT LIKE '%{slug}_01_1280x720_h264%'")
+            f"SELECT * FROM storage_file WHERE path::TEXT LIKE '%{slug}_01%' AND path::TEXT LIKE '%_h264_%' ORDER BY size DESC")
+
         video = cursor.fetchone()
 
         return self.makeurl(video)
 
     def get_lecture_thumbnail(self, slug):
-
+        """ try: """
         cursor.execute(
-            f"SELECT * FROM storage_file WHERE \"path\"::TEXT LIKE '%{slug}_01.ss%'")
+            f"SELECT * FROM storage_file WHERE path::TEXT LIKE '%{slug}%' AND ext::TEXT LIKE 'jpg' ORDER BY size")
         img = cursor.fetchone()
 
         return self.makeurl(img)
+        """ except:
+        return '' """
 
     def get_lectures(self):
+        cursor.execute("Select * FROM vl_lecture LIMIT 0")
+        LECTURE_COLS = [desc[0] for desc in cursor.description]
+
         cursor.execute(
             "SELECT * FROM vl_lecture WHERE (\"enabled\" = 'true') AND (\"type\" = 'vl') AND (\"public\" = 'true')")
         lectures = cursor.fetchall()
 
         added = 0
         already_there = 0
+        errors = 0
 
         for lecture in tqdm(lectures):
             lec_id = lecture[0]
@@ -113,10 +139,11 @@ class Command(BaseCommand):
                 Lecture.objects.get(id=lec_id)
                 already_there += 1
             except:
-                slug = lecture[1]
-                title = lecture[2]
-                description = lecture[8]
-                published = lecture[24]
+                slug = lecture[LECTURE_COLS.index('slug')]
+                title = lecture[LECTURE_COLS.index('title')]
+                description = lecture[LECTURE_COLS.index('desc')]
+                published = lecture[LECTURE_COLS.index('pub_date')]
+                views = lecture[LECTURE_COLS.index('view_ctr')]
 
                 try:
 
@@ -124,18 +151,29 @@ class Command(BaseCommand):
 
                     thumbnail = self.get_lecture_thumbnail(slug)
 
-                    author = self.get_lecture_author_id(lec_id)
+                    try:
+                        author = self.get_lecture_author_id(lec_id)
+                    except:
+                        author = Author.objects.get(id=1)
+
+                    author.views += views
+                    author.save()
 
                     Lecture.objects.create(id=lec_id, title=title, description=description,
                                            author=author, video=video_url, published=published,
-                                           thumbnail=thumbnail, views=666, audio='https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3'
+                                           thumbnail=thumbnail, views=views, audio='https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3'
                                            )
 
                     added += 1
 
                 except Exception as e:
+                    errors += 1
+                    tqdm.write(
+                        f'Error at: {slug}. Currently {errors} errors. \n{e}')
                     pass
-        print(f'Added {added} lectures and updated {already_there} lectures')
+
+        print(
+            f'Added {added} lectures and updated {already_there} lectures; {errors} errors.')
 
     def get_authors(self):
         cursor.execute("SELECT * FROM vl_author")
@@ -147,14 +185,24 @@ class Command(BaseCommand):
         cursor.execute("SELECT * FROM vl_contributor WHERE \"type\" = 'P'")
         authors = cursor.fetchall()
 
+        cursor.execute("Select * FROM vl_contributor LIMIT 0")
+        cols = [desc[0] for desc in cursor.description]
+
+        try:
+            Author.objects.get(id=1)
+        except:
+            Author.objects.update_or_create(
+                id=1, name='none', views=0)
+
         print('Adding authors')
         for author in tqdm(authors):
+            aut_id = author[cols.index('id')]
             # print(author)
             try:
-                Author.objects.get(id=author[0])
+                Author.objects.get(id=aut_id)
             except:
                 Author.objects.create(
-                    id=author[0], name=author[2], views=0, description=descriptions[author[0]])
+                    id=aut_id, name=author[cols.index('name')], views=0, description=descriptions[author[aut_id]])
         print('Added all authors')
 
     def process_categories(self):
@@ -163,18 +211,22 @@ class Command(BaseCommand):
 
         df = pd.DataFrame(categories)
 
+        cursor.execute("Select * FROM categories_category LIMIT 0")
+
+        cols = [desc[0] for desc in cursor.description]
+
         # ? sort by url_path -> can create straight away
-        df = df.sort_values(4)
+        df = df.sort_values(cols.index('url_path'))
 
         print('Adding categories')
         for index, cat in tqdm(df.iterrows()):
             # schema as seen on production_DB
-            parent_id = self.ToInt(cat[2])
+            parent_id = self.ToInt(cat[cols.index('parent_id')])
             parent = None
             if parent_id:
                 parent = Category.objects.get(id=parent_id)
             Category.objects.update_or_create(
-                id=cat[0], name=cat[1], parent=parent)
+                id=cat[cols.index('id')], name=cat[cols.index('name')], parent=parent)
         print('Added all categories')
 
         return True
@@ -188,19 +240,31 @@ class Command(BaseCommand):
         updated = 0
         created = 0
 
+        cursor.execute("Select * FROM auth_user LIMIT 0")
+
+        cols = [desc[0] for desc in cursor.description]
+
         for user in tqdm(users):
             # print(author)
 
+            user_id = user[cols.index('id')]
+
+            password = user[cols.index('password')]
+
             try:
-                userObject = User.objects.get(id=user[0])
-                if userObject.password != user[5]:
-                    userObject.password = user[5]
+                userObject = User.objects.get(id=user_id)
+                if userObject.password != password:
+                    userObject.password = password
 
                     updated += 1
                     userObject.save()
             except:
-                User.objects.create(id=user[0], username=user[1], first_name=user[2], last_name=user[3], email=user[4], password=user[5], is_staff=user[6], is_active=user[7],
-                                    is_superuser=user[8])
+                User.objects.create(id=user_id, username=user[cols.index('username')], first_name=user[cols.index('first_name')],
+                                    last_name=user[cols.index(
+                                        'last_name')], email=user[cols.index('email')],
+                                    password=password, is_staff=user[cols.index(
+                                        'is_staff')], is_active=user[cols.index('is_active')],
+                                    is_superuser=user[cols.index('is_superuser')])
                 created += 1
 
         # first_name=user[2], last_name=user[3], email = user[4], last_login=user[9], date_joined=user[10]
@@ -209,12 +273,16 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-        self.process_categories()
-        self.get_authors()
-        self.get_users()
+        # self.process_categories()
+        # self.get_authors()
+        # self.get_users()
 
         self.get_lectures()
 
         # print('test')
+
+        """ lec = 'fin_grobelnik_uvod_v_velike_podatke'
+        print(self.get_lecture_video(lec))
+        print(self.get_lecture_thumbnail(lec)) """
 
         server.stop()
