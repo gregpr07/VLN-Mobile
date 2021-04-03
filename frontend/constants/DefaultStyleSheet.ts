8b7bea928267b59a04206698eb38ce69223827f0
Gregor Zunic <gregor.ninjaboy@gmail.com>
2021-04-03 22:28:55 +0200

    fixed android shadows

--- frontend/constants/DefaultStyleSheet.ts
+++ frontend/constants/DefaultStyleSheet.ts
@@ -1,4 +1,4 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Platform } from "react-native";
 
 export const padding_const = 10;
 
@@ -6,13 +6,20 @@ export const border_radius = 15;
 
 const defaultStyles = StyleSheet.create({
   shadow: {
-    shadowColor: "#6B7280",
-    shadowOffset: {
-      width: 0,
-      height: 4,
-    },
-    shadowRadius: 3,
-    shadowOpacity: 0.1,
+    ...Platform.select({
+      ios: {
+        shadowColor: "#6B7280",
+        shadowOffset: {
+          width: 0,
+          height: 4,
+        },
+        shadowRadius: 3,
+        shadowOpacity: 0.1,
+      },
+      android: {
+        elevation: 2,
+      },
+    }),
   },
 });
 
