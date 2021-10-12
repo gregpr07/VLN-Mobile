e86987c4e2a4d6565c926c73c0cc85dadc72e39c
Gregor Zunic <gregor.ninjaboy@gmail.com>
2021-10-12 12:13:43 +0200

    url change admin backedn

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -6,8 +6,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('api/admin/', admin.site.urls),
-    path('api/', include('api.urls')),
-    path('api/search/', include('esearch.urls')),
-    url(r'^api/auth/', include('knox.urls')),
+    path("admin/", admin.site.urls),
+    path("api/", include("api.urls")),
+    path("api/search/", include("esearch.urls")),
+    url(r"^api/auth/", include("knox.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
